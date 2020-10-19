a6711755f8caf451bef0e4be4608608ada4000a5
tienluc <khongphaitui25@gmail.com>
2020-10-19 22:39:16 +0700

    form admin from mixi with love :)))

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,38 +1,40 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { TablesComponent } from './pages/tables/tables.component';
-import { FormsComponent } from './pages/forms/forms.component';
-import { TypographyComponent } from './pages/typography/typography.component';
-import { MapsComponent } from './pages/maps/maps.component';
-import { NotificationsComponent } from './pages/notifications/notifications.component';
-import { StaffComponent } from './pages/staff/staff.component';
-import { AddStaffComponent } from './pages/add-staff/add-staff.component';
-import { UpdateStaffComponent } from './pages/update-staff/update-staff.component';
-import { CustomerManagerComponent } from './pages/customer-manager/customer-manager.component';
-import { CarManagerComponent } from './pages/car-manager/car-manager.component';
-import { CarAddComponent } from './pages/car-add/car-add.component';
-import { CarUpdateComponent } from './pages/car-update/car-update.component';
-import { BrandManagerComponent } from './pages/brand-manager/brand-manager.component';
-import { BrandAddComponent } from './pages/brand-add/brand-add.component';
-import { BrandUpdateComponent } from './pages/brand-update/brand-update.component';
-import { FeatureManagerComponent } from './pages/feature-manager/feature-manager.component';
-import { FeatureAddComponent } from './pages/feature-add/feature-add.component';
-import { FeatureUpdateComponent } from './pages/feature-update/feature-update.component';
-import { ModelManagerComponent } from './pages/model-manager/model-manager.component';
-import { ModelAddComponent } from './pages/model-add/model-add.component';
-import { ModelUpdateComponent } from './pages/model-update/model-update.component';
-import { IndexAdminComponent } from './pages/index-admin/index-admin.component';
-import { LoginAdminComponent } from './pages/login-admin/login-admin.component';
-import { RegisterAdminComponent } from './pages/register-admin/register-admin.component';
+import { DashboardComponent } from './Admin/dashboard/dashboard.component';
+import { TablesComponent } from './Admin/tables/tables.component';
+import { FormsComponent } from './Admin/forms/forms.component';
+import { TypographyComponent } from './Admin/typography/typography.component';
+import { MapsComponent } from './Admin/maps/maps.component';
+import { NotificationsComponent } from './Admin/notifications/notifications.component';
+import { StaffComponent } from './Admin/staff/staff.component';
+import { AddStaffComponent } from './Admin/add-staff/add-staff.component';
+import { UpdateStaffComponent } from './Admin/update-staff/update-staff.component';
+import { CustomerManagerComponent } from './Admin/customer-manager/customer-manager.component';
+import { CarManagerComponent } from './Admin/car-manager/car-manager.component';
+import { CarAddComponent } from './Admin/car-add/car-add.component';
+import { CarUpdateComponent } from './Admin/car-update/car-update.component';
+import { BrandManagerComponent } from './Admin/brand-manager/brand-manager.component';
+import { BrandAddComponent } from './Admin/brand-add/brand-add.component';
+import { BrandUpdateComponent } from './Admin/brand-update/brand-update.component';
+import { FeatureManagerComponent } from './Admin/feature-manager/feature-manager.component';
+import { FeatureAddComponent } from './Admin/feature-add/feature-add.component';
+import { FeatureUpdateComponent } from './Admin/feature-update/feature-update.component';
+import { ModelManagerComponent } from './Admin/model-manager/model-manager.component';
+import { ModelAddComponent } from './Admin/model-add/model-add.component';
+import { ModelUpdateComponent } from './Admin/model-update/model-update.component';
+import { IndexAdminComponent } from './Admin/index-admin/index-admin.component';
+import { LoginAdminComponent } from './Admin/login-admin/login-admin.component';
+import { RegisterAdminComponent } from './Admin/register-admin/register-admin.component';
+
+
 
 
 const routes: Routes = [
-  {path: 'admin',   redirectTo: 'login', pathMatch: 'full'},
+
+  {path: 'admin',   redirectTo: 'register', pathMatch: 'full'},
   {path: 'login', component: LoginAdminComponent},
   {path: 'register', component: RegisterAdminComponent},
-
-
+  
   {path: '',   redirectTo: 'index', pathMatch: 'full'},
   {path: 'index', component: IndexAdminComponent, children:[
   {path: '',   redirectTo: 'dashboard', pathMatch: 'full'},
